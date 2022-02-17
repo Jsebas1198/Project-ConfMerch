3cd5c8b7fcd60e1c1016d57f3787f0888fd8f8b9
Juan Fernandez <jsebas1198@protonmail.com>
2022-02-17 17:07:38 -0300

    finish the UI of checkout, information, payment and success

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -3,7 +3,24 @@ import "../styles/components/Checkout.css"
 
 const Checkout = () => {
   return (
-    <div>Checkout</div>
+    <div className="Checkout">
+        <div className="Checkout-content">
+            <h3>Lista de pedidos:</h3>
+            <div className="Checkout-item">
+                <div className="Checkout-element">
+                  <h4>Item name</h4>
+                  <span>$12</span>
+                </div>
+                <button type='button'>Eliminar</button>
+            </div>
+
+        </div>
+
+        <div className="Checkout-sidebar">
+          <h3>Precio total: $1231</h3>
+          <button type='button'> Continuar pedido</button>
+        </div>
+    </div>
   )
 }
 
--- src/containers/Success .jsx
+++ src/containers/Success .jsx
@@ -3,7 +3,15 @@ import "../styles/components/Success.css"
 
 const Success  = () => {
   return (
-    <div>Success </div>
+    <div className="Success">
+      <div className="Success-content">
+        <h2>Juan, Gracias por tu compra</h2>
+        <span>Tu pedido lelgara en 3 dias a tu direccion:</span>
+        <div className="Success-map">
+          Google Maps
+        </div>
+      </div>
+    </div>
   )
 }
 
