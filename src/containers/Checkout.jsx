68e8bedabbf28238763aa96eb32bc480f2f21324
Juan Fernandez <jsebas1198@protonmail.com>
2022-02-18 16:41:06 -0300

    on the web the user can add products to the cart and delete them from the order,modified useInitialState and checkout

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,33 +1,48 @@
-import React from 'react';
-import {Link} from "react-router-dom";
-import "../styles/components/Checkout.css"
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import AppContext from '../context/AppContext';
+import '../styles/components/Checkout.css';
 
 const Checkout = () => {
+  const { state, removeFromCart } = useContext(AppContext);
+  const { cart } = state;
+
+  const handleRemove = (product, i) => () => {
+    removeFromCart(product, i);
+ };
+
+  const handleSumTotal = () => {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue.price;
+    const sum = cart.reduce(reducer, 0);
+    return sum;
+  }
+
   return (
     <div className="Checkout">
-        <div className="Checkout-content">
-            <h3>Lista de pedidos:</h3>
-            <div className="Checkout-item">
-                <div className="Checkout-element">
-                  <h4>Item name</h4>
-                  <span>$12</span>
-                </div>
-                <button type='button'>
-                <i class="fa-solid fa-trash-can"></i>
-                </button>
+      <div className="Checkout-content">
+        {cart.length > 0 ? <h3>Lista de pedidos:</h3> : <h3>Sin pedidos...</h3>}
+        {cart.map((item, i) => (
+          <div className="Checkout-item">
+            <div className="Checkout-element">
+              <h4>{item.title}</h4>
+              <span>{item.price}</span>
             </div>
-
-        </div>
-
+            <button type="button" onClick={handleRemove(item, i)}>
+              <i className="fas fa-trash-alt" />
+            </button>
+          </div>
+        ))}
+      </div>
+      {cart.length > 0 && (
         <div className="Checkout-sidebar">
-          <h3>Precio total: $1231</h3>
-
+          <h3>{`Precio Total: $ ${handleSumTotal()}`}</h3>
           <Link to="/checkout/information">
-            <button type='button'> Continuar pedido</button>
+            <button type="button">Continuar pedido</button>
           </Link>
         </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Checkout
+export default Checkout;
